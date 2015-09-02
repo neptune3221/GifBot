8ebd28138e2d473da9b4859321aae9ddbad6bec6
Carlos Bolanos <neptune@Carloss-MacBook-Pro.local>
2015-09-01 22:46:23 -0400

    added more gifs

--- bot.js
+++ bot.js
@@ -5,11 +5,50 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /\/drift compatible/;
+      botRegex1 = /\/drift compatible/;
+      botRegex2 = /\/bitch please/;
+      botRegex3 = /\whatever/;
+      botRegex4 = /\I don't give a fuck/;
+      botRegex5 = /\deal with it/;
+      botRegex6 = /\my nigga/;
+      message;
+      link;
 
   if(request.text && botRegex.test(request.text)) {
+    message = "drift compatible";
+    link = "http://i.groupme.com/245x165.gif.b000fb258d0246c0bc175fe0d089029e.large"
     this.res.writeHead(200);
-    postMessage();
+    postMessage(message, link);
+    this.res.end();
+  } else if(request.text && botRegex2.test(request.text)) {
+    message = "bitch please";
+    link = "http://i.groupme.com/500x250.gif.e3d764eb8d9a45ea8202a4bc14c50452.large";
+    this.res.writeHead(200);
+    postMessage(message, link);
+    this.res.end();
+  } else if(request.text && botRegex3.test(request.text)) {
+    message = "whatever";
+    link = "http://i.groupme.com/500x281.gif.145407d9c6024cd1a181abf628e7392e.large";
+    this.res.writeHead(200);
+    postMessage(message, link);
+    this.res.end();
+  } else if(request.text && botRegex4.test(request.text)) {
+    message = "I don't give a fuck";
+    link = "http://i.groupme.com/432x243.gif.2589579794d14a5f8c3c1f44d22b9797.large";
+    this.res.writeHead(200);
+    postMessage(message, link);
+    this.res.end();
+  } else if(request.text && botRegex5.test(request.text)) {
+    message = "deal with it";
+    link = "http://i.groupme.com/500x273.gif.c1e27e8ac06a44ea9df62eb53b491cee.large";
+    this.res.writeHead(200);
+    postMessage(message, link);
+    this.res.end();
+  } else if(request.text && botRegex6.test(request.text)) {
+    message = "my nigga";
+    link = "http://i.groupme.com/552x464.jpeg.1e322ce495f24bc3ae65fe3dc9dc32d7.large";
+    this.res.writeHead(200);
+    postMessage(message, link);
     this.res.end();
   } else {
     console.log(/*"don't care"*/ request.text);
@@ -19,12 +58,12 @@ function respond() {
 }
 
 
-//todo: create post image to groupme image server function
+
 
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = "drift compatible"; // change this to picture variable
+  botResponse = message; // change this to picture variable
 
   options = {
     hostname: 'api.groupme.com',
@@ -38,7 +77,7 @@ function postMessage() {
     "attachments" : [
        {
           "type" : "image",
-          "url"  : "http://i.groupme.com/245x165.gif.b000fb258d0246c0bc175fe0d089029e.large"
+          "url"  : link
        }
     ]
   };
