405ef0a9e6e154d7fba7f5ecab0ca89019a641e1
Carlos Bolanos <neptune@Carloss-MacBook-Pro.local>
2019-12-07 00:06:09 -0500

    testing

--- bot.js
+++ bot.js
@@ -39,7 +39,7 @@ function respond() {
       botRegex32 = /\/djkhaled/;
       botRegex33 = /\/feels/;
       botRegex34 = /\/[Ss]irius/;
-      botRegex35 = /\/[Gg]iphy/; 
+      botRegex35 = /\/[Gg]if/; 
       botRegex36 = /\/[Ss]urprise/;
       botRegex37 = /\/[Ww]hat/;
       botRegex38 = /\/[Ll]/;
@@ -252,7 +252,7 @@ function respond() {
     postMessage(message, link);
     this.res.end();
   } else if(request.text && botRegex35.test(request.text)) {
-    botRegex35 = request.text.split("/giphy ");
+    botRegex35 = request.text.split("/gif ");
     message = botRegex35[1];
     console.log(message);
     this.res.writeHead(200);
@@ -434,51 +434,5 @@ function postMessageGiphy() {
   });
   giphyReq.end();
   
-
-  //botResponse = giphyReq.end(JSON.parse(data).url)
-  
-  // request.onload = function() {
-  //   if (request.status >= 200 && request.status < 400){
-  //     botResponse = JSON.parse(request.responseText).data.image_url;
-  //     console.log(botResponse);
-  //   } else {
-  //     console.log('reached giphy, but API returned an error');
-  //    }
-  // };
-
-  // request.onerror = function() {
-  //   console.log('connection error');
-  // };
-
-  // request.send();
-
-  // options = {
-  //   hostname: 'api.groupme.com',
-  //   path: '/v3/bots/post',
-  //   method: 'POST'
-  // };
-
-  // body = {
-  //   "bot_id" : botID,
-  //   "text" : imageResponse,
-  // };
-
-  // console.log('sending ' + message + ' to ' + botID);
-
-  // botReq = HTTPS.request(options, function(res) {
-  //     if(res.statusCode == 202) {
-  //       //neat
-  //     } else {
-  //       console.log('rejecting bad status code ' + res.statusCode);
-  //     }
-  // });
-
-  // botReq.on('error', function(err) {
-  //   console.log('error posting message '  + JSON.stringify(err));
-  // });
-  // botReq.on('timeout', function(err) {
-  //   console.log('timeout posting message '  + JSON.stringify(err));
-  // });
-  // botReq.end(JSON.stringify(body));
 }
 exports.respond = respond;
