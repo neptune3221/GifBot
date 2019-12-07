d6e566d37cb9d716a69b56c5b1b67d04c501b294
Carlos Bolanos <neptune@Carloss-MacBook-Pro.local>
2019-12-06 20:31:26 -0500

    testing

--- bot.js
+++ bot.js
@@ -256,7 +256,8 @@ function respond() {
     message = botRegex35[1];
     console.log(message);
     this.res.writeHead(200);
-    postMessageGiphy(message);
+    link = postMessageGiphy(message);
+    postMessage(message, link);
     this.res.end();  
   }  else if(request.text && botRegex36.test(request.text)) {
     message = "surprise";
@@ -401,6 +402,7 @@ function postMessageGiphy() {
     console.log('timeout posting message '  + JSON.stringify(err));
   });
   giphyReq.end()
+  return imageResponse;
 
   //botResponse = giphyReq.end(JSON.parse(data).url)
   
@@ -419,33 +421,33 @@ function postMessageGiphy() {
 
   // request.send();
 
-  options = {
-    hostname: 'api.groupme.com',
-    path: '/v3/bots/post',
-    method: 'POST'
-  };
+  // options = {
+  //   hostname: 'api.groupme.com',
+  //   path: '/v3/bots/post',
+  //   method: 'POST'
+  // };
 
-  body = {
-    "bot_id" : botID,
-    "text" : imageResponse,
-  };
+  // body = {
+  //   "bot_id" : botID,
+  //   "text" : imageResponse,
+  // };
 
-  console.log('sending ' + message + ' to ' + botID);
+  // console.log('sending ' + message + ' to ' + botID);
 
-  botReq = HTTPS.request(options, function(res) {
-      if(res.statusCode == 202) {
-        //neat
-      } else {
-        console.log('rejecting bad status code ' + res.statusCode);
-      }
-  });
+  // botReq = HTTPS.request(options, function(res) {
+  //     if(res.statusCode == 202) {
+  //       //neat
+  //     } else {
+  //       console.log('rejecting bad status code ' + res.statusCode);
+  //     }
+  // });
 
-  botReq.on('error', function(err) {
-    console.log('error posting message '  + JSON.stringify(err));
-  });
-  botReq.on('timeout', function(err) {
-    console.log('timeout posting message '  + JSON.stringify(err));
-  });
-  botReq.end(JSON.stringify(body));
+  // botReq.on('error', function(err) {
+  //   console.log('error posting message '  + JSON.stringify(err));
+  // });
+  // botReq.on('timeout', function(err) {
+  //   console.log('timeout posting message '  + JSON.stringify(err));
+  // });
+  // botReq.end(JSON.stringify(body));
 }
 exports.respond = respond;
