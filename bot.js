a081d1af7ce7fa6cb4700e07360a17a66c4b774a
Carlos Bolanos <neptune@Carloss-MacBook-Pro.local>
2019-12-06 22:36:29 -0500

    testing

--- bot.js
+++ bot.js
@@ -389,7 +389,6 @@ function postMessageGiphy() {
           console.log(data);
           console.log(botResponse.data[0].url);
           var link = botResponse.data[0].url;
-          postMessageHelp(link);
         });
     
   });
@@ -401,7 +400,36 @@ function postMessageGiphy() {
   giphyReq.on('timeout', function(err) {
     console.log('timeout posting message '  + JSON.stringify(err));
   });
-  giphyReq.end()
+  giphyReq.end(function() {
+    options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : link,
+  };
+
+  console.log('sending ' + link + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+  })
   
 
   //botResponse = giphyReq.end(JSON.parse(data).url)
